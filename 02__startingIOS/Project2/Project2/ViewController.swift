a43d1d46bfc8220eb27e3241e7f552b4a3b7de4b
Jamie Brannan <jamiebrannan2@gmail.com>
2020-06-25 15:51:26 +0200

    [p2] Correct answer set for question batch

--- 02__startingIOS/Project2/Project2/ViewController.swift
+++ 02__startingIOS/Project2/Project2/ViewController.swift
@@ -16,15 +16,16 @@ class ViewController: UIViewController {
   
   var countries = [String]()
   var score = 0
+  var correctAnswer = 0
   
   override func viewDidLoad() {
     super.viewDidLoad()
     countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-    
+
     button1.layer.borderWidth = 1
     button2.layer.borderWidth = 1
     button3.layer.borderWidth = 1
-    
+
     button1.layer.borderColor = UIColor.lightGray.cgColor
     button2.layer.borderColor = UIColor.lightGray.cgColor
     button3.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
@@ -35,9 +36,17 @@ class ViewController: UIViewController {
   /// Callup flags based on their names `UIImage(named:)`
   func askQuestion() {
     countries.shuffle()
+
+    /**
+     Correct answer must be randomly one of the three chosen to display
+     */
+    correctAnswer = Int.random(in: 0...2)
+
     button1.setImage(UIImage(named: countries[0]), for: .normal)
     button2.setImage(UIImage(named: countries[1]), for: .normal)
     button3.setImage(UIImage(named: countries[2]), for: .normal)
+
+    title = countries[correctAnswer].uppercased()
   }
 }
 
