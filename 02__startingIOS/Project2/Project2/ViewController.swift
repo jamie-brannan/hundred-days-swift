47dc63cde35855fde516ac367912fcec81b795aa
Jamie Brannan <jamiebrannan2@gmail.com>
2020-06-25 15:43:44 +0200

    [p2] Randomized flag selection

--- 02__startingIOS/Project2/Project2/ViewController.swift
+++ 02__startingIOS/Project2/Project2/ViewController.swift
@@ -9,34 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
   @IBOutlet var button1: UIButton!
   @IBOutlet var button2: UIButton!
   @IBOutlet var button3: UIButton!
-
+  
   var countries = [String]()
   var score = 0
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
-
+    
     button1.layer.borderWidth = 1
     button2.layer.borderWidth = 1
     button3.layer.borderWidth = 1
-
+    
     button1.layer.borderColor = UIColor.lightGray.cgColor
     button2.layer.borderColor = UIColor.lightGray.cgColor
     button3.layer.borderColor = UIColor(red: 1.0, green: 0.6, blue: 0.2, alpha: 1.0).cgColor
-
+    
     askQuestion()
   }
-
+  
   /// Callup flags based on their names `UIImage(named:)`
   func askQuestion() {
-      button1.setImage(UIImage(named: countries[0]), for: .normal)
-      button2.setImage(UIImage(named: countries[1]), for: .normal)
-      button3.setImage(UIImage(named: countries[2]), for: .normal)
+    countries.shuffle()
+    button1.setImage(UIImage(named: countries[0]), for: .normal)
+    button2.setImage(UIImage(named: countries[1]), for: .normal)
+    button3.setImage(UIImage(named: countries[2]), for: .normal)
   }
 }
 
