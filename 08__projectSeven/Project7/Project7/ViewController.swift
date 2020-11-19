e82bd721c9edbc6066bc0a43a667530086f2e9c3
Jamie Brannan <jamiebrannan2@gmail.com>
2020-11-19 15:48:10 +0100

    [p7] table with petitions data

--- 08__projectSeven/Project7/Project7/ViewController.swift
+++ 08__projectSeven/Project7/Project7/ViewController.swift
@@ -7,13 +7,40 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
-
+class TableViewController: UITableViewController {
+  
+  var petitions = [Petition]()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+    let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+    
+    if let url = URL(string: urlString) {
+      if let data = try? Data(contentsOf: url) {
+        parse(json: data)
+      }
+    }
   }
-
-
+  
+  func parse(json: Data) {
+    let decoder = JSONDecoder()
+    
+    if let jsonPetitions = try? decoder.decode(Petitions.self, from: json) {
+      petitions = jsonPetitions.results
+      tableView.reloadData()
+    }
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return petitions.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "thingy", for: indexPath)
+    let petition = petitions[indexPath.row]
+    cell.textLabel?.text = petition.title
+    cell.detailTextLabel?.text = petition.body
+    return cell
+  }
 }
-
