1e62fe1ec3924f90db910208a8fedcdc87d5fdd3
Jamie Brannan <jamiebrannan2@gmail.com>
2020-12-13 20:49:36 +0100

    [challenge] credit button complete

--- 08__projectSeven/Project7/Project7/ViewController.swift
+++ 08__projectSeven/Project7/Project7/ViewController.swift
@@ -15,7 +15,7 @@ class TableViewController: UITableViewController {
     super.viewDidLoad()
     // let urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
     let urlString: String
-
+    setupNavigation()
     if navigationController?.tabBarItem.tag == 0 {
         // urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
         urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
@@ -23,20 +23,18 @@ class TableViewController: UITableViewController {
   
         urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
     }
-    setupNavigation()
     if let url = URL(string: urlString) {
         if let data = try? Data(contentsOf: url) {
             parse(json: data)
             return
         }
     }
-
     showError()
   }
   
   func setupNavigation() {
     let clearButton = UIBarButtonItem(title: "Clear", style: .plain, target: self, action: nil)
-    let creditButton = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: nil)
+    let creditButton = UIBarButtonItem(title: "Credits", style: .plain, target: self, action: #selector(showCredits))
     navigationItem.leftBarButtonItem = clearButton
     navigationItem.rightBarButtonItem = creditButton
   }
@@ -54,6 +52,12 @@ class TableViewController: UITableViewController {
       let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
       ac.addAction(UIAlertAction(title: "OK", style: .default))
       present(ac, animated: true)
+  }
+  
+  @objc func showCredits() {
+    let ac = UIAlertController(title: "Credits", message: "The following data is provided by the We The People API of the Whitehouse", preferredStyle: .alert)
+    ac.addAction(UIAlertAction(title: "OK", style: .default))
+    present(ac, animated: true)
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- 08__projectSeven/d035__notes.md
+++ 08__projectSeven/d035__notes.md
@@ -73,9 +73,9 @@
 >
 > :one:  Add a Credits button to the top-right corner using `UIBarButtonItem`. When this is tapped, show **an alert** telling users the data comes from the We The People API of the Whitehouse.
 
-  - [ ]  add upper right nav button
-  - [ ]  selector to present an alert
-  - [ ]  enter proper text
+  - [x]  add upper right nav button
+  - [x]  selector to present an alert
+  - [x]  enter proper text
 
 > :two: Let users filter the petitions they see. This involves creating a **second array of filtered items** that contains only **petitions matching a string the user entered**. Use a `UIAlertController` with a text field to let them enter that string. This is a tough one, so I’ve included some hints below if you get stuck.
 
