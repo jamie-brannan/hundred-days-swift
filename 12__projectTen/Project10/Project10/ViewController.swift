55676407cbbbe2a62bc872e95d9ed8ecb5c28608
Jamie Brannan <jamiebrannan2@gmail.com>
2021-02-04 17:04:57 +0100

    [p10][challenge] add camera directly

--- 12__projectTen/Project10/Project10/ViewController.swift
+++ 12__projectTen/Project10/Project10/ViewController.swift
@@ -24,15 +24,19 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
   @objc func addNewPerson() {
     let picker = UIImagePickerController()
     /// allows the user to crop the picture they select
+    guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+      return
+    }
+    picker.sourceType = .camera
     picker.allowsEditing = true
     picker.delegate = self
     present(picker, animated: true)
   }
-
+  
   // MARK: - Actions
 
   // MARK:  Navigation bar
-
+  
   func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
     /// try to read the image, and typecast it as an image for the interface – and  if it's not `guard` will let us bail out
     guard let image = info[.editedImage] as? UIImage else { return }
@@ -61,7 +65,7 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
     return paths[0]
   }
-
+  
   // MARK: - Callbacks
   
   override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -69,23 +73,23 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-          fatalError("Unable to dequeue PersonCell.")
-      }
-
-      let person = people[indexPath.item]
-
-      cell.nameLabel.text = person.name
-
-      let path = getDocumentsDirectory().appendingPathComponent(person.image)
-      cell.imageView.image = UIImage(contentsOfFile: path.path)
-
-      cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
-      cell.imageView.layer.borderWidth = 2
-      cell.imageView.layer.cornerRadius = 3
-      cell.layer.cornerRadius = 7
-
-      return cell
+    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+      fatalError("Unable to dequeue PersonCell.")
+    }
+    
+    let person = people[indexPath.item]
+    
+    cell.nameLabel.text = person.name
+    
+    let path = getDocumentsDirectory().appendingPathComponent(person.image)
+    cell.imageView.image = UIImage(contentsOfFile: path.path)
+    
+    cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+    cell.imageView.layer.borderWidth = 2
+    cell.imageView.layer.cornerRadius = 3
+    cell.layer.cornerRadius = 7
+    
+    return cell
   }
   
   override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -96,11 +100,16 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
     
-    ac.addAction(UIAlertAction(title: "Rename", style: .default) { [weak self, weak ac] _ in
-      guard let newName = ac?.textFields?[0].text else { return }
-      person.name = newName
-      
-      self?.collectionView.reloadData()
+    ac.addAction(UIAlertAction(title: "Rename", style: .default) { _ in
+      let act = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+      act.addTextField()
+      act.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak act] _ in
+        guard let newName = act?.textFields?[0].text else { return }
+        person.name = newName
+
+        self?.collectionView.reloadData()
+      })
+      self.present(act, animated: true)
     })
     
     ac.addAction(UIAlertAction(title: "Delete", style: .default) {_ in
--- 12__projectTen/d044__notes.md
+++ 12__projectTen/d044__notes.md
@@ -58,9 +58,9 @@ Add an action the preceeds the current renaming alert.
 ```
 
 
->  - [ ]  Try using `picker.sourceType = .camera` when creating your image picker, **which will tell it to create a new image by taking a photo.** This is only available on devices (not on the simulator) so you might want to check the return value of `UIImagePickerController.isSourceTypeAvailable()` before trying to use it!
+>  - [x]  Try using `picker.sourceType = .camera` when creating your image picker, **which will tell it to create a new image by taking a photo.** This is only available on devices (not on the simulator) so you might want to check the return value of `UIImagePickerController.isSourceTypeAvailable()` before trying to use it!
 
-  - [ ]  test on device
+  - [x]  test on device
 
 >   - [ ]  Modify project 1 so that it uses a collection view controller rather than a table view controller. I recommend you keep a copy of your original table view controller code so you can refer back to it later on.
 
