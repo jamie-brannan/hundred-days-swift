2d99e70384f312468a3f734536ada5d3e7822dbe
Jamie Brannan <jamiebrannan2@gmail.com>
2021-02-04 14:36:08 +0100

    [p10] rename cell

--- 12__projectTen/Project10/Project10/ViewController.swift
+++ 12__projectTen/Project10/Project10/ViewController.swift
@@ -65,13 +65,41 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
   }
   
   override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
-      // we failed to get a PersonCell – bail out!
-      fatalError("Unable to dequeue PersonCell.")
-    }
-    
-    // if we're still here it means we got a PersonCell, so we can return it
-    return cell
+      guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+          fatalError("Unable to dequeue PersonCell.")
+      }
+
+      let person = people[indexPath.item]
+
+      cell.nameLabel.text = person.name
+
+      let path = getDocumentsDirectory().appendingPathComponent(person.image)
+      cell.imageView.image = UIImage(contentsOfFile: path.path)
+
+      cell.imageView.layer.borderColor = UIColor(white: 0, alpha: 0.3).cgColor
+      cell.imageView.layer.borderWidth = 2
+      cell.imageView.layer.cornerRadius = 3
+      cell.layer.cornerRadius = 7
+
+      return cell
+  }
+  
+  override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+      let person = people[indexPath.item]
+
+      let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+      ac.addTextField()
+
+      ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+
+      ac.addAction(UIAlertAction(title: "OK", style: .default) { [weak self, weak ac] _ in
+          guard let newName = ac?.textFields?[0].text else { return }
+          person.name = newName
+
+          self?.collectionView.reloadData()
+      })
+
+      present(ac, animated: true)
   }
 }
 
