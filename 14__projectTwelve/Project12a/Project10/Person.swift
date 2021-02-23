eaf0b9d220bed9d6efc4d08a7431c94b5a36db32
Jamie Brannan <jamiebrannan2@gmail.com>
2021-02-23 08:36:34 +0100

    [p12a] add coder and decoder

--- 14__projectTwelve/Project12a/Project10/Person.swift
+++ 14__projectTwelve/Project12a/Project10/Person.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class Person: NSObject {
+class Person: NSObject, NSCoding {
   var name: String
   var image: String
   
@@ -15,5 +15,16 @@ class Person: NSObject {
   init(name: String, image: String) {
       self.name = name
       self.image = image
+  }
+
+  /// required to inherit`NSCoding`
+  required init(coder aDecoder: NSCoder) {
+      name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+      image = aDecoder.decodeObject(forKey: "image") as? String ?? ""
+  }
+
+  func encode(with aCoder: NSCoder) {
+      aCoder.encode(name, forKey: "name")
+      aCoder.encode(image, forKey: "image")
   }
 }
