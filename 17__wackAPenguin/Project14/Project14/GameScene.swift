b2961fbfe884b6c52ff89ed7356b1de6010389a5
Jamie Brannan <jamiebrannan2@gmail.com>
2021-05-10 16:35:40 +0200

    [p14][challenge2] added final score tabulation

--- 17__wackAPenguin/Project14/Project14/GameScene.swift
+++ 17__wackAPenguin/Project14/Project14/GameScene.swift
@@ -90,7 +90,15 @@ class GameScene: SKScene {
       gameOver.position = CGPoint(x: 512, y: 384)
       gameOver.zPosition = 1
       addChild(gameOver)
-            
+
+      let finalScore = SKLabelNode(fontNamed: "Chalkduster")
+      finalScore.text = "Final Score • \(score)"
+      finalScore.position = CGPoint(x: 512, y: 300)
+      finalScore.zPosition = 2
+      finalScore.horizontalAlignmentMode = .center
+      finalScore.fontSize = 48
+      addChild(finalScore)
+
       return
     }
     
