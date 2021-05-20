45e1f9c4ddc8490549ecb82f0a63bc4bca32c5f8
Jamie Brannan <jamiebrannan2@gmail.com>
2021-05-20 15:25:06 +0200

    [pConsolidationVi] empty project

---
+++ 19__consolidationVi/ConsolidationVI/ConsolidationVI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsolidationVI
+//
+//  Created by Jamie Brannan on 20/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
