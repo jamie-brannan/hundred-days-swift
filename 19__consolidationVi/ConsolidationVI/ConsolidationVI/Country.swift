d03e63ee1c857bcb80448506baa88606e5ae97df
Jamie Brannan <jamiebrannan2@gmail.com>
2021-05-20 16:41:18 +0200

    [pConsolidationVi] add data structures and views

---
+++ 19__consolidationVi/ConsolidationVI/ConsolidationVI/Country.swift
@@ -0,0 +1,14 @@
+//
+//  Country.swift
+//  ConsolidationVI
+//
+//  Created by Jamie Brannan on 20/05/2021.
+//
+
+import Foundation
+
+struct Country: Codable {
+  var motto: String
+  var language: String
+  var captial: String
+}
