10f656ad79ecb963711342536f1531127f9453bd
Jamie Brannan <jamiebrannan2@gmail.com>
2021-05-20 23:09:56 +0200

    [pConsolidationVi] detail display all country data
    layout and detail page design to be handled later

--- 19__consolidationVi/ConsolidationVI/ConsolidationVI/CountryDetailViewController.swift
+++ 19__consolidationVi/ConsolidationVI/ConsolidationVI/CountryDetailViewController.swift
@@ -14,8 +14,24 @@ class CountryDetailViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "detailCell")
     if let country = country {
       self.navigationItem.title = country.name
     }
+  }
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath)
+    guard let motto = country?.motto, let language = country?.language, let capital = country?.capital else { return cell }
+    cell.textLabel?.text = "\(motto), \(language), \(capital)"
+    return cell
   }
 }
