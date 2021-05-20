79e3f29ec66ab93dab0de39665b4f046836f795d
Jamie Brannan <jamiebrannan2@gmail.com>
2021-05-20 18:44:01 +0200

    [pConsolidationVi] adjust data structure and json

--- 19__consolidationVi/ConsolidationVI/ConsolidationVI/Country.swift
+++ 19__consolidationVi/ConsolidationVI/ConsolidationVI/Country.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 struct Country: Codable {
+  var name: String
   var motto: String
   var language: String
-  var captial: String
+  var capital: String
 }
--- 19__consolidationVi/ConsolidationVI/ConsolidationVI/ViewController.swift
+++ 19__consolidationVi/ConsolidationVI/ConsolidationVI/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+
+  var countriesList = [Country]()
+
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    getListOfCountries()
   }
 
+  func getListOfCountries() {
+    guard let urlPath = Bundle.main.url(forResource: "countries", withExtension: "json") else { return }
+    guard let data = try? Data(contentsOf: urlPath) else { return }
+    parse(json: data)
+  }
 
+  func parse(json: Data) {
+    do {
+      let decoder = JSONDecoder()
+      let decodedResults = try decoder.decode(Countries.self, from: json)
+      self.countriesList = decodedResults.results
+    } catch let error {
+      print(error)
+    }
+  }
 }
-
