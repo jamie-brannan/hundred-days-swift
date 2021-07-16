223c3c7793b45ccdf245361bcfb5d86c3042cdef
Jamie Brannan <jamiebrannan2@gmail.com>
2021-07-16 22:58:30 +0200

    [consolidationVii] create target row class

--- ch14__consolidation_vii/ConsolidationVii/ConsolidationVii/GameScene.swift
+++ ch14__consolidation_vii/ConsolidationVii/ConsolidationVii/GameScene.swift
@@ -10,17 +10,15 @@ import GameplayKit
 
 class GameScene: SKScene {
 
+
   override func didMove(to view: SKView) {
     backgroundColor = .yellow
-    addRow(at: CGPoint(x: frame.midX, y: frame.midY + frame.height/3 ), with: .cyan)
-    addRow(at: CGPoint(x: frame.midX, y: frame.midY), with: .brown)
-    addRow(at: CGPoint(x: frame.midX, y: frame.midY - frame.height/3 ), with: .cyan)
-  }
-
-  func addRow(at position: CGPoint, with color: UIColor) {
-    let row = SKShapeNode(rectOf: CGSize(width: frame.width, height: (frame.height / 3)))
-    row.position = position
-    row.fillColor = color
-    addChild(row)
+    let rowSize = CGSize(width: frame.width, height: frame.height/3)
+    let topRow = TargetRow(direction: .left, fill: .cyan, size: rowSize, point: CGPoint(x: frame.midX, y: frame.midY + frame.height/3 ), named: .top)
+    let middleRow = TargetRow(direction: .right, fill: .brown, size: rowSize, point: CGPoint(x: frame.midX, y: frame.midY), named: .middle)
+    let bottomRow = TargetRow(direction: .right, fill: .cyan, size: rowSize, point: CGPoint(x: frame.midX, y: frame.midY - frame.height/3 ), named: .bottom)
+    addChild(topRow)
+    addChild(middleRow)
+    addChild(bottomRow)
   }
 }
