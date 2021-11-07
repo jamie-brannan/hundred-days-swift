1a71dac85b0fd749fc7ced642a5d0632190b939e
Jamie Brannan <jamiebrannan2@gmail.com>
2021-11-07 21:34:35 +0100

    [p19] complete

--- ch15__levelingUp/Project19/Extension/ActionViewController.swift
+++ ch15__levelingUp/Project19/Extension/ActionViewController.swift
@@ -11,25 +11,39 @@ import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
   
-  @IBOutlet weak var imageView: UIImageView!
+  @IBOutlet var script: UITextView!
+  var pageTitle = ""
+  var pageURL = ""
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(done))
+    
     if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
       if let itemProvider = inputItem.attachments?.first {
         itemProvider.loadItem(forTypeIdentifier: kUTTypePropertyList as String) { [weak self] (dict, error) in
           guard let itemDictionary = dict as? NSDictionary else { return }
           guard let javaScriptValues = itemDictionary[NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
-          print(javaScriptValues)
+          self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+          self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+          
+          DispatchQueue.main.async {
+            self?.title = self?.pageTitle
+          }
         }
       }
     }
   }
   
   @IBAction func done() {
-    // Return any edited content to the host app.
-    // This template doesn't do anything, so we just echo the passed in items.
-    self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    let item = NSExtensionItem()
+    let argument: NSDictionary = ["customJavaScript": script.text]
+    let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+    let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+    item.attachments = [customJavaScript]
+
+    extensionContext?.completeRequest(returningItems: [item])
   }
   
 }
