ccf055d53b4b5843e878ece27cae2f5614ac7a6c
Jamie Brannan <jamiebrannan2@gmail.com>
2021-11-18 15:57:39 +0100

    [p20] init

---
+++ ch15__levelingUp/Project20/Project20/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project20
+//
+//  Created by Jamie Brannan on 14/11/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+  override func didMove(to view: SKView) {
+    
+  }
+}
