9ff2da422486a523096eebb25f21dbd873bdab48
Jamie Brannan <jamiebrannan2@gmail.com>
2021-11-18 15:59:57 +0100

    [p20] add background

--- ch15__levelingUp/Project20/Project20/GameScene.swift
+++ ch15__levelingUp/Project20/Project20/GameScene.swift
@@ -10,6 +10,10 @@ import SpriteKit
 class GameScene: SKScene {
 
   override func didMove(to view: SKView) {
-    
+    let background = SKSpriteNode(imageNamed: "background")
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
   }
 }
