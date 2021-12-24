8ff3c2abbc020375a4aa9d859984b5d18097084f
Jamie Brannan <jamiebrannan2@gmail.com>
2021-12-24 19:12:31 +0100

    [consolidationviii] add nav over a table view

---
+++ ch15__levelingUp/ConsolidationViii/ConsolidationViii/MainTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTableViewController.swift
+//  ConsolidationViii
+//
+//  Created by Jamie Brannan on 24/12/2021.
+//
+
+import UIKit
+
+class MainTableViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Notepad"
+    navigationController?.navigationBar.prefersLargeTitles = true
+  }
+
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    var cell = UITableViewCell(style: .subtitle, reuseIdentifier: "note")
+    cell.textLabel?.text = "tiiiiiiitle"
+    cell.detailTextLabel?.text = "suuuuuuuuuubtitle"
+    return cell
+  }
+}
+
