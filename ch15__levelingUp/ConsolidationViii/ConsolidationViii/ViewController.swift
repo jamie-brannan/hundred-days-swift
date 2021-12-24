08f65fb63c8740427fbd946ad00c2ad5abb961c1
Jamie Brannan <jamiebrannan2@gmail.com>
2021-12-24 18:41:44 +0100

    [consolidationviii] init project

---
+++ ch15__levelingUp/ConsolidationViii/ConsolidationViii/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConsolidationViii
+//
+//  Created by Jamie Brannan on 24/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
