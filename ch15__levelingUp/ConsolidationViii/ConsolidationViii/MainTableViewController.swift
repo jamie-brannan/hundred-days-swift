fecd5f12aa9111960759ff2686eb5c1d845c4898
Jamie Brannan <jamiebrannan2@gmail.com>
2021-12-26 12:14:47 +0100

    [consolidationviii] create and implement a custom data type

--- ch15__levelingUp/ConsolidationViii/ConsolidationViii/MainTableViewController.swift
+++ ch15__levelingUp/ConsolidationViii/ConsolidationViii/MainTableViewController.swift
@@ -9,18 +9,36 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
 
+  // MARK: - Properties
+  var fakeData = [
+    NotepadNote(title: "first", body: "fakenote"),
+    NotepadNote(title: "second", body: "fakenote"),
+    NotepadNote(title: "third", body: "fakenote"),
+  ]
+
+  // MARK: - Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Notepad"
     navigationController?.navigationBar.prefersLargeTitles = true
   }
 
+  // MARK: - Table View
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 1
+    return fakeData.count
   }
 
+  // MARK: Table Cell Management
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    var cell = UITableViewCell(style: .subtitle, reuseIdentifier: "note")
+    let data = fakeData[indexPath.row]
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "note")
+    cell.textLabel?.text = data.title
+    cell.detailTextLabel?.text = data.body
+    return cell
+  }
+
+  private func returnSubstituteCell() -> UITableViewCell {
+    let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "note")
     cell.textLabel?.text = "tiiiiiiitle"
     cell.detailTextLabel?.text = "suuuuuuuuuubtitle"
     return cell
---
+++ ch15__levelingUp/ConsolidationViii/ConsolidationViii/NotepadNote.swift
@@ -0,0 +1,13 @@
+//
+//  NotepadNote.swift
+//  ConsolidationViii
+//
+//  Created by Jamie Brannan on 26/12/2021.
+//
+
+import Foundation
+
+struct NotepadNote: Codable {
+  var title: String
+  var body: String
+}
