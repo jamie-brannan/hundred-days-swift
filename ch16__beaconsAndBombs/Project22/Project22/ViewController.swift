cf9510de927064409b05e31aa1cb6b787d6ad902
Jamie Brannan <jamiebrannan2@gmail.com>
2022-01-06 18:00:38 +0100

    [p22] init

---
+++ ch16__beaconsAndBombs/Project22/Project22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22
+//
+//  Created by Jamie Brannan on 06/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
